eed3dcb36591b2a0ad69ecc0befdf3bb45072d0f
zhanglong <zhanglong@weipaitang.com>
2021-01-15 11:10:22 +0800

    feat: 修改打包和发布文件

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import { uglify } from "rollup-plugin-uglify";
 export default {
   input: "./src/index.js", // 以哪个文件作为打包入口
   output: {
-    file: "index.js", // 出口路径
+    file: "dist/index.js", // 出口路径
     name: "JSUtil", // 指定打包后全局变量的名字
     format: "umd", // 统一模块规范
     sourcemap: true, // 开启源码调试
